bc92129c4cdf78f0e25605225704e5c4a2a5edc1
ying-tonkotsu <kurenatsue@gmail.com>
2020-04-27 15:14:25 +0900

    larn prpps component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
 import React from 'react';
 
-// function App() {
 
-//   return(
-//     <React.Fragment>
-//       <label htmlFor="bar">bar</label>
-//       <input type="text" onChange={() => {console.log("I am changed")}}></input>
-//     </React.Fragment>
-//   )}
 
 const App = () => {
+  const profiles = [
+    {
+      name: "Taro",
+      age: 10,
+    },
+    {
+      name: "Hanako",
+      age: 5,
+    },
+    {
+      name: "Noname",
+      // age: 5,
+    },
+  ];
   return (
-  <div>
-    <Cat/>
-  </div>
-  )
+    <div>
+      {
+        profiles.map((profile,index) => {
+          return <User name={profile.name} age={profile.age} key={index} />;
+        })
+      }
+    </div>
+  );
 }
-const Cat = () => {
-  return <div>Meow</div>;
+const User = (props) => {
+return <div>Hi!,I am {props.name},and {props.age} yeas old.</div>
 };
+
+User.defaultProps = {
+  age:1
+}
+
 export default App;
